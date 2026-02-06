a9139fc4eb4d4fec32a2c7dceb924f4081ec1855
Aethryns <alex.anderson@ucas-edu.net>
2026-02-06 13:28:26 -0700

    Implement time-based random number logic
    Add functions to check current second and modify a value based on conditions.

--- randomizer.cpp
+++ randomizer.cpp
@@ -1,1 +1,33 @@
+#include <ctime>
+#include <iostream>
+using namespace std
+// c = current percent
+// i = ideal percent
+// o = overall percent
+// p = percent change
 
+bool isCurrentSecondEven() {
+    std::time_t now = std::time(nullptr);
+    std::tm* local_time = std::localtime(&now);
+    
+    // Returns true if even, false if odd
+    return (local_time->tm_sec % 2 == 0);
+  }
+
+int RandomNumberPicker({) {
+  for 
+}
+
+bool tof(float i,float p){
+  float c = 50;
+  float o;
+  if (isCurrentSecondEven()){
+    if (c >= i){
+      c-=p;
+    }else{
+      c+=p;
+    }
+  }else{
+    if 
+  }
+}
